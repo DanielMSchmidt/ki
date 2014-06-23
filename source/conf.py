cf14352a34b7688c3756f4bf0ad6ac0704ae4bb5
Daniel Schmidt <dschmidt@weluse.de>
2014-06-23 13:45:09 +0200

    changed date of copyright

--- source/conf.py
+++ source/conf.py
@@ -48,7 +48,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'KI'
-copyright = u'2013, Daniel Schmidt'
+copyright = u'2014, Daniel Schmidt'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
